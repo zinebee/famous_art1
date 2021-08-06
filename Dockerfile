966db7675e1327512012cdd2791fb7c93c6535e8
zineb el <elzinou95@gmail.com>
2021-08-06 19:57:12 +0100

    exodevops4

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:16
+FROM openjdk:11
 EXPOSE 8080
 ADD target/famousart.jar  famousart.jar  
 ENTRYPOINT ["java","-jar","/famousart.jar"]
--- src/main/java/org/famousart/modele/Car.java
+++ src/main/java/org/famousart/modele/Car.java
@@ -3,5 +3,5 @@ package org.famousart.modele;
 public class Car {
  private int idcar;
  private String a;
- private String b;
+ private String bss;
 }
